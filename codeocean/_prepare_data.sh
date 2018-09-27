27011d2a74736a42002a1bce3e2f683ad32e48ac
Karel Břinda <karel.brinda@gmail.com>
2018-09-27 16:49:12 -0400

    Add downloading metadata

--- codeocean/_prepare_data.sh
+++ codeocean/_prepare_data.sh
@@ -4,17 +4,40 @@ set -e
 set -o pipefail
 set -u
 
-d=$(mktemp -d)
 
+rm -fr data
+mkdir data
+
+# 1) download metadata
+
+(
+	cd data
+	for x in SPARC.core_genes.tree isolates.2013.tsv isolates.2015.tsv; do
+		wget https://raw.githubusercontent.com/c2-d2/rase-db/466bc84318616950cd479c85bd86cb0bc31be321/spneumoniae_sparc/published/$x
+	done
+)
+
+
+# 2) create isolates.tar
+
+mkdir data/isolates
+d=$(mktemp -d)
 (
 	cd "$d"
 	git clone http://github.com/c2-d2/rase-db
 )
+(
+	cd data/isolates
+	"$d"/rase-db/spneumoniae_sparc/isolates/download_sparc1_assemblies.sh
+)
+(
+	cd data
+	tar cvf isolates.tar isolates
+	rm -fr isolates/
+)
 
-rm -fr data
-mkdir data
-mkdir data/isolates
 
+# 3) download nanopore reads from Zenodo
 (
 	cd data
 	curl https://zenodo.org/record/1405173 \
@@ -25,13 +48,3 @@ mkdir data/isolates
 	rm list.txt
 )
 
-(
-	cd data/isolates
-	"$d"/rase-db/spneumoniae_sparc/isolates/download_sparc1_assemblies.sh
-)
-
-(
-	cd data
-	tar cvf isolates.tar isolates
-	rm -fr isolates/
-)
