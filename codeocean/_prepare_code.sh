a354032d87bd4c0077b63360583062d221f10e3a
Karel Břinda <karel.brinda@gmail.com>
2018-09-27 16:52:07 -0400

    Add full cleaning of the repositories

--- codeocean/_prepare_code.sh
+++ codeocean/_prepare_code.sh
@@ -10,6 +10,8 @@ d=$(mktemp -d)
 	cd "$d"
 	git clone http://github.com/c2-d2/rase-db
 	git clone http://github.com/c2-d2/rase-predict
+	make -C rase-db/spneumoniae_sparc cleanall
+	make -C rase-predict cleanall
 	rm -fr rase-db/.git
 	rm -fr rase-predict/.git
 )
